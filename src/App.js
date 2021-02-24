dea9664550a253e59b70d8072bb9a4131c1a111a
Md A Awal Hadi <awalhadi5@gmail.com>
2021-02-24 21:52:17 +0600

    react component, destructure, useState, object pass

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const friends = [
+    {name: "Saddam", job: "worker"},
+    {name: "Rasel", job: "job finder"},
+    {name: "Bappi", job: "job finder"},
+    {name: "Shawkot", job: "Student"},
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <p>Learning react</p>
+        <Count></Count>
+        {
+          friends.map(friend => <Person friend={friend}></Person>)
+        }
+       
       </header>
+    </div>
+  );
+}
+
+function Count() {
+  const [count, setCount] = useState(0);
+  const increaseHandlar = () => setCount(count + 1);
+  return(
+    <div>
+      <h2>Count : {count}</h2>
+      <button onClick={increaseHandlar}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Deccrease</button>
+    </div>
+  )
+}
+
+
+
+function Person(props){
+  const {name, job} = props.friend;
+  return (
+    <div style={{border:"2px solid gray", padding:"5px", margin:"10px",borderRadius:"5px", width:"400px"}}>
+      <h1>Name : {name}</h1>
+      <p>My Profession : {job}</p>
     </div>
   );
 }
